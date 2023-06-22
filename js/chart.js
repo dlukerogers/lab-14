e578dff334b26f17f2247ae12fc9069c388a9851
Geoff Lloyd <orionblack@gmail.com>
2023-06-22 15:03:41 -0700

    uploading changes while Geoff drove

--- js/chart.js
+++ js/chart.js
@@ -11,6 +11,44 @@ let canvasElem = document.getElementById('chart')
  *
  */
 function renderChart() {
+  let results = new AppState();
+  results.loadItems();
+  let dataObject = results.allProducts;
+  let numberVotes = [];
+  let numberViews = [];
+  let labelNames = [];
+  for (let i = 0; i < dataObject.length; i++){
+    labelNames.push(dataObject[i].name);
+    numberVotes.push(dataObject[i].timesClicked);
+    numberViews.push(dataObject[i].timesShown);
+  }
+  new Chart(canvasElem, {
+    type: 'bar',
+    data: {
+      labels: labelNames,
+      datasets: [{
+        label: '# of Votes',
+        data: numberVotes,
+        borderWidth: 2,
+        backgroundColor: '#0147AB',
+        borderColor: '#000080'
+      },
+      {
+        label: '# of Views',
+        data: numberViews,
+        borderWidth: 2,
+        backgroundColor: '#A45EE9',
+        borderColor: '#4B0076'
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
 }
 
 renderChart();
